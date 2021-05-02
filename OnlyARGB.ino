c28972716003b95cecfb9779075e36cd12955986
Kahloyx <kahloyx@gmail.com>
2021-05-02 02:32:32 +0200

    Separated the themes and the main sketch

--- OnlyARGB.ino
+++ OnlyARGB.ino
@@ -6,7 +6,7 @@ int maxPalette = 4;     // This is the number of registrered palette we got
 int argbSwitch = 5;     // This is the command of the relay
 
 #define LED_PIN     3       // This is the signal pin for the leds
-#define NUM_LEDS    60       // This is in fact 9 + 37 + 14
+#define NUM_LEDS    60       // This is in fact 9(Headset support) + 37(Behind the screen) + 14(Inside the PC case)
 #define BRIGHTNESS  188        // Was first at 64 then 16
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
@@ -31,7 +31,7 @@ void setup() {
   }
 
   uint32_t currentFrequency;
-	pinMode(argbSwitch, OUTPUT);
+  pinMode(argbSwitch, OUTPUT);
   int j = 0;
   while (j < 2) {               // Just an init as we love'em
     fill_solid( currentPalette, 16, CRGB::Grey);
@@ -45,92 +45,65 @@ void setup() {
   }
   Serial.println("Hello!");
 }
+
+
+
 void loop () {
- 
-  while(!Serial){
-	digitalWrite(argbSwitch, HIGH);
-} 
+
+  while (!Serial) {
+    digitalWrite(argbSwitch, HIGH);
+  }
+
   static uint8_t startIndex = 0;
-  if (currpal == 1){
+  if (currpal == 1) {
     FirstPalette();
-  startIndex = startIndex + 1; /* motion speed */
-
-  FillLEDsFromPaletteColors( startIndex);
+    startIndex = startIndex + 1; /* motion speed */
+    FillLEDsFromPaletteColors( startIndex);
   }
-  if (currpal == 2){
+
+  if (currpal == 2) {
     SecondPalette();
-  startIndex = startIndex + 1; /* motion speed */
-
-  FillLEDsFromPaletteColors( startIndex);
+    startIndex = startIndex + 1; /* motion speed */
+    FillLEDsFromPaletteColors( startIndex);
   }
-  if (currpal == 3){
+
+  if (currpal == 3) {
     ThirdPalette();
   }
-  if (currpal == 4){
-    FourthPalette();
-  startIndex = startIndex + 1; /* motion speed */
 
-  FillLEDsFromPaletteColors( startIndex);
+  if (currpal == 4) {
+    FourthPalette();
+    startIndex = startIndex + 1; /* motion speed */
+    FillLEDsFromPaletteColors( startIndex);
   }
-  if (currpal > maxPalette){    // This way we cycle throught the palette we got
+
+  if (currpal > maxPalette) {   // This way we cycle throught the palette we got
     currpal = 1;
   }
-
   FastLED.show();
   FastLED.delay(120 / UPDATES_PER_SECOND);
 
 
-                            /*                       COMMUNICATION
-                             *                        
-                             *                        
-                             *                        
-                             *                        
-                             *                        
-                             *                       COMMUNICATION
-                             */
+  /*                       COMMUNICATION
+
+
+
+
+
+                           COMMUNICATION
+  */
   if ( Serial.available()) {     // Check if any char available on serial, if there's any instructions
     char c = Serial.read();
-    if (c == 's'){
+    if (c == 's') {
       Serial.println(c);
       Serial.println("Switching.");
-      currpal = currpal +1;
+      currpal = currpal + 1;
     }
-    if (c == 'n'){
+    if (c == 'n') {
       Serial.println("Switching on");
     }
-    if (c == 'f'){
+    if (c == 'f') {
       Serial.println("Switching off");
     }
   }
 }
-
-// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
-// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-
-
-
-// Additionl notes on FastLED compact palettes:
-//
-// Normally, in computer graphics, the palette (or "color lookup table")
-// has 256 entries, each containing a specific 24-bit RGB color.  You can then
-// index into the color palette using a simple 8-bit (one byte) value.
-// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
-// is quite possibly "too many" bytes.
-//
-// FastLED does offer traditional 256-element palettes, for setups that
-// can afford the 768-byte cost in RAM.
-//
-// However, FastLED also offers a compact alternative.  FastLED offers
-// palettes that store 16 distinct entries, but can be accessed AS IF
-// they actually have 256 entries; this is accomplished by interpolating
-// between the 16 explicit entries to create fifteen intermediate palette
-// entries between each pair.
-//
-// So for example, if you set the first two explicit entries of a compact
-// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved
-// the first sixteen entries from the virtual palette (of 256), you'd get
-// Green, followed by a smooth gradient from green-to-blue, and then Blue.
---
+++ RGBThemes.ino
@@ -0,0 +1,63 @@
+void FirstPalette()
+{
+  CRGB c1 = CHSV( HUE_RED, 255, 255);
+  CRGB c2 = CHSV( HUE_ORANGE, 255, 255);
+  CRGB c3 = CHSV( HUE_YELLOW, 255, 255);
+  CRGB c4 = CHSV( HUE_GREEN, 255, 255);
+  CRGB c5 = CHSV( HUE_AQUA, 255, 255);
+  CRGB c6 = CHSV( HUE_BLUE, 255, 255);
+  CRGB c7 = CHSV( HUE_PURPLE, 255, 255);
+  CRGB c8 = CHSV( HUE_PINK, 255, 255);
+  // 'black out' all 16 palette entries...
+  fill_solid( currentPalette, 16, CRGB::Black);
+  currentPalette[0] = c1;
+  currentPalette[2] = c2;
+  currentPalette[4] = c3;
+  currentPalette[6] = c4;
+  currentPalette[8] = c5;
+  currentPalette[10] = c6;
+  currentPalette[12] = c7;
+  currentPalette[14] = c8;
+
+}
+
+void SecondPalette()                 // What I got in This Function is a half and half circle cyan and pink, way too much aestethicc and the speed is calculated and smooth enought.
+{
+  CRGB c1 = CHSV( HUE_AQUA, 255, 255);
+  CRGB c2 = CHSV( HUE_PINK, 255, 255);
+  //fill_solid( currentPalette, 16, c2);
+
+  currentPalette = CRGBPalette16(
+                     c1, c1, c2, c2,
+                     c1, c1, c2, c2,
+                     c1, c1, c2, c2,
+                     c1, c1, c2, c2 );
+}
+void ThirdPalette()
+{
+  fill_solid(leds, 60, CRGB(255, 0, 188));
+  FastLED.show();
+}
+
+void FourthPalette()
+{
+  CRGB c1 = CHSV(HUE_YELLOW, 255, 255);
+  CRGB c2 = CHSV(HUE_PINK, 255, 255);
+  fill_solid( currentPalette, 16, CRGB::Black);
+  currentPalette[0] = c1;
+  currentPalette[2] = c2;
+  currentPalette[5] = c1;
+  currentPalette[8] = c2;
+  currentPalette[11] = c1;
+  currentPalette[14] = c2;
+}
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+  uint8_t brightness = 255;
+
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+    colorIndex += 3;
+  }
+}
